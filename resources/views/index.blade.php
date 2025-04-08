51248e1aacad24837c243c974d4e4ee194e2c8a8
HikkaZXC <m.kovidyaev@yandex.ru>
2025-04-08 20:56:38 +0500

    Добавлены стили для секции на главной странице и обновлено представление с информацией о портале клининговых услуг, включая кнопку регистрации.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,15 @@
 
 @section('content')
 
-
+    <section style="align-items: center; height: 90vh;">
+        <div style="display: flex; flex-direction: column; align-items: center; gap: 30px">
+            <h1 style="text-align: center">
+                Портал клининговых услуг «Мой Не Сам» представляет собой информационную систему для формирования заявок на
+                уборку жилых и производственных помещений. Перед началом использования портала пользователю необходимо
+                пройти процедуру регистрации
+            </h1>
+            <a href="{{ route('registerView') }}">Регистрация</a>
+        </div>
+    </section>
 
 @endsection
